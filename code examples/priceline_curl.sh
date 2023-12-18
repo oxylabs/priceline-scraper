a64adf72fe40b376ac7118a76b40c49946ac94a6
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 12:38:23 +0200

    Adding code examples/priceline_curl.sh

---
+++ code examples/priceline_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.priceline.com/hotels?semprod=brand&refid=plgooglecpcbsl&refclickid=d%3ac%7c10941049094%7c460243569023%7cpriceline%7ce%7cg%7ckwd-10842441%7c9062284&gad_source=1&gclid=cj0kcqiayewrbhddarisagp1mwt8yb2nqslfysgnfq4tumcnvwpbyxa4bm_m6g-xxhg1ieghhdtyleyaasddealw_wcb&slingshot=1852"}'
